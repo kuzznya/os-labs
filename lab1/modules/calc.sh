eb4bdc28a8d6d2eba9400b93807a578673bada38
Ilya Kuznetsov <ikuz2002@gmail.com>
2020-09-16 12:16:37 +0300

    feat: Add params check

--- lab1/modules/calc.sh
+++ lab1/modules/calc.sh
@@ -3,6 +3,7 @@
 [[ -z "$IMPORT_CALC" ]] && IMPORT_CALC=true || return 0
 
 require core.sh
+require errors.sh
 
 sum() {
     let "result = $1 + $2"
@@ -24,10 +25,22 @@ div() {
     echo $result
 }
 
+# is_int <param>
+# Returns 0 if param is int, else -1
+is_int() {
+    [[ "$1" =~ ^([1-9][0-9]*|0)$ ]] && return 0 || return -1
+}
+
+# calc <action> <int1> <int2>
+# action: sum/sub/mul/div
+# If action is unknown, then invalid_args throws
 calc() {
     local action="$1"
     local param1="$2"
     local param2="$3"
+    
+    ! is_int $param1 && invalid_args "calc" "$param1 is not an int"
+    ! is_int $param2 && invalid_args "calc" "$param2 is not an int"
 
     case $action in
 	sum )
