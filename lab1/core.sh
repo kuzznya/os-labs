b7936e48e476ce2c64977a80e640c500b8eb55b6
Ilya Kuznetsov <ikuz2002@gmail.com>
2020-09-16 10:34:42 +0300

    refactor: Rename utils to core & edit errors

---
+++ lab1/core.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+[[ -z "$IMPORT_CORE" ]] && IMPORT_CORE=true || return 0
+
+# import <script path>
+# Load script from path
+# Return 0 if script was loaded or -1 if not
+import() {
+    if [[ -f "$1" ]] ; then
+	source "$1" && return 0 || return -1
+    else
+	return -1
+    fi
+}
+
+#require <script path>
+# Load script from path or call missing_script
+require() {
+    if ! import errors.sh ; then
+	echo "Fatal error: missing script errors.sh" > /dev/stderr && exit -10
+    fi
+
+    if ! import "$1" ; then
+	echo Fuck
+	missing_script "$1"
+    fi
+}
+
+# Print program manual
+print_man() {
+    cat man.txt
+}
