5272e38819127eca203f6cbed3765fd648460656
Ilya Kuznetsov <ikuz2002@gmail.com>
2020-10-02 00:10:35 +0300

    chore: Add output info about native calls

--- lab2/utils/src/main/cpp/os_socket_Socket.cpp
+++ lab2/utils/src/main/cpp/os_socket_Socket.cpp
@@ -4,15 +4,19 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <memory>
+
 #include <iostream>
 
 JNIEXPORT jint JNICALL Java_os_socket_Socket_socket
         (JNIEnv* env, jobject obj, jshort domain, jint type, jint protocol) {
+    std::cout << "socket(): Defining socket with domain " << domain << ", type " << type << std::endl;
     return socket(domain, type, protocol);
 }
 
 JNIEXPORT jint JNICALL Java_os_socket_Socket_bind
         (JNIEnv* env, jobject obj, jint socket, jshort domain, jcharArray address) {
+    std::cout << "bind(): Binding socket " << socket << ", domain " << domain << std::endl;
+
     jboolean isCopy = true;
     char* addressData = reinterpret_cast<char*>(
             env->GetCharArrayElements(address, &isCopy)
@@ -30,5 +34,7 @@ JNIEXPORT jint JNICALL Java_os_socket_Socket_bind
 
 JNIEXPORT jint JNICALL Java_os_socket_Socket_close
         (JNIEnv* env, jobject obj, jint socket) {
+    std::cout << "close(): Closing socket " << socket << std::endl;
+
     return close(socket);
 }
