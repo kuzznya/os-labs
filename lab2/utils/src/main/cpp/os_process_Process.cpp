c1d2704cf511c0c57be5fb80e88861d176ffe1f6
Theodore Basmanov <chatta54@gmail.com>
2020-10-13 20:20:46 +0300

    Corrected String editing

--- lab2/utils/src/main/cpp/os_process_Process.cpp
+++ lab2/utils/src/main/cpp/os_process_Process.cpp
@@ -2,7 +2,7 @@
 #include <process.h>
 #include <string.h>
 
-unsigned char* as_unsigned_char_array(jbyteArray array) {
+unsigned char* as_unsigned_char_array(JNIEnv * env, jbyteArray array) {
     int len = env->GetArrayLength (array);
     unsigned char* buf = new unsigned char[len];
     env->GetByteArrayRegion (array, 0, len, reinterpret_cast<jbyte*>(buf));
@@ -11,8 +11,8 @@ unsigned char* as_unsigned_char_array(jbyteArray array) {
 
 JNIEXPORT jint JNICALL Java_os_process_Process_run
   (JNIEnv * env, jclass c, jbyteArray byteArray){
-       char * const wholeLine[] = as_unsigned_char_array(byteArray);
+       char * const wholeLine[] = as_unsigned_char_array(env, byteArray);
        char * const name[] = strtok(wholeLine, " ");
-       char * const arguements[] = strtok(wholeLine, " ");
+       char * const arguements[] = strtok(NULL, " ");
        return execv(name, arguements);
   }
