bf4bfd87ca093d4c0d24b60b408984187bf71db5
Theodore Basmanov <chatta54@gmail.com>
2020-10-13 16:26:28 +0300

    Created a new brunch

--- lab2/utils/src/main/java/os/process/Process.java
+++ lab2/utils/src/main/java/os/process/Process.java
@@ -37,4 +37,8 @@ public class Process {
     public void kill() {
         Runtime.kill(PID);
     }
+    public static int run(String name){
+        return run(name.getBytes());
+    }
+    private static native int run(byte[] name);
 }
